9bf42df922fe94b85fb817db26a8c941717829db
taka010116 <166984313+taka010116@users.noreply.github.com>
2025-09-23 17:56:46 +0900

    First commit

--- app/__init__.py
+++ app/__init__.py
@@ -3,12 +3,16 @@ from flask import Flask
 from .routes import main
 from config import Config
 
+socketio = SocketIO(cors_allowed_origins="*")
+
 def create_app():
     app = Flask(__name__)
 
     app.config.from_object(Config)
+    from app.routes import main
     app.register_blueprint(main)
 
+    socketio.init_app(app)
     return app
 
 app = create_app()
--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,6 @@
 from flask import Blueprint, redirect, url_for, Flask, jsonify, render_template, flash, render_template_string, request,g, session
+from flask_socketio import emit
+from app import socketio
 
 main = Blueprint('main', __name__)
 
@@ -6,3 +8,9 @@ main = Blueprint('main', __name__)
 def index():
     
     return render_template('index.html')
+
+@socketio.on("message")
+def handle_message(data):
+    print("受信:", data)
+    emit("message", data, broadcast=True)  # 全員に送信
+
