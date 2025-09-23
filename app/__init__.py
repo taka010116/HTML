67492d89f5b33329126d4852e81c2e8877149583
taka010116 <166984313+taka010116@users.noreply.github.com>
2025-09-23 22:45:29 +0900

    Fix socketio init and update requirements

--- app/__init__.py
+++ app/__init__.py
@@ -5,25 +5,27 @@ from flask_socketio import SocketIO
 from config import Config
 
 #socketio = SocketIO(cors_allowed_origins="*")
-socketio = SocketIO()
+#socketio = SocketIO()
+socketio = SocketIO(cors_allowed_origins="*")
 
 def create_app():
     app = Flask(__name__)
-
     app.config.from_object(Config)
     from app.routes import main
     app.register_blueprint(main)
 
     socketio.init_app(app)
-
-    @socketio.on("message")
-    def handle_message(data):
-        print("受信:", data)
-        socketio.emit("message", {"msg": f"サーバーが受け取った: {data}"}, broadcast=True)
+    
     return app
 
 app = create_app()
+@socketio.on("message")
+def handle_message(data):
+    print("受信:", data)
+    socketio.emit("message", {"msg": data}, broadcast=True)
+
 
 if __name__ == "__main__":
     #app.run()
-    socketio.run(app)
+    #socketio.run(app)
+    socketio.run(app, host="0.0.0.0", port=5000)
